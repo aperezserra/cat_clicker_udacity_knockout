ed75e28f91cef88eebf1a79b7b1da293b93187b0
Abraham Perez Serra <aperez@EEM.loc>
2018-05-17 20:53:21 -0400

    Playing with Observable Arrays, the lines was tested and then commented in the html file

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ var ViewModel = function() {
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://github.com/udacity/ud989-cat-clicker-ko-starter/tree/master/img');
 
+    this.testArray = ko.observableArray(["Cuco","Tato","Garabato"]);
+
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
     };
