b234b4bc810065ab864055ed57e46cbb67961300
aperezserra <abraham.perezserra@gmail.com>
2018-05-19 00:57:17 -0400

    List off clickble cats added with dummy function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,48 @@
+var initalCats = [
+    {
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution: 'https://github.com/udacity/ud989-cat-clicker-ko-starter/tree/master/img',
+        nickname: ["Cuco"],
+    },
+    {
+        clickCount: 0,
+        name: 'Johan',
+        imgSrc: 'img/22252709_010df3379e_z.jpg',
+        imgAttribution: 'https://github.com/udacity/ud989-cat-clicker-ko-starter/tree/master/img',
+        nickname: ["Tito"],
+    },
+    {
+        clickCount: 0,
+        name: 'Cricket',
+        imgSrc: 'img/1413379559_412a540d29_z.jpg',
+        imgAttribution: 'https://github.com/udacity/ud989-cat-clicker-ko-starter/tree/master/img',
+        nickname: ["Coco"],
+    },
+    {
+        clickCount: 0,
+        name: 'Kuchiki',
+        imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+        imgAttribution: 'https://github.com/udacity/ud989-cat-clicker-ko-starter/tree/master/img',
+        nickname: ["Garabato"],
+    },
+    {
+        clickCount: 0,
+        name: 'Ichigo',
+        imgSrc: 'img/9648464288_2516b35537_z.jpg',
+        imgAttribution: 'https://github.com/udacity/ud989-cat-clicker-ko-starter/tree/master/img',
+        nickname: ["El Flecha"],
+    },
+
+];
+
 var Cat = function(data) {
     this.clickCount = ko.observable(data.clickCount);
     this.name = ko.observable(data.name);
     this.imgSrc = ko.observable(data.imgSrc);
     this.imgAttribution = ko.observable(data.imgAttribution);
-    this.catList = ko.observableArray(data.catList);
+    this.nickname = ko.observableArray(data.nickname);
 
     // this.testArray = ko.observableArray(["Cuco","Tato","Garabato"]);
 
@@ -23,16 +62,22 @@ var Cat = function(data) {
 
 var ViewModel = function() {
 
-    this.currentCat = ko.observable( new Cat({
-        clickCount: 0,
-        name: 'Tabby',
-        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-        imgAttribution: 'https://github.com/udacity/ud989-cat-clicker-ko-starter/tree/master/img',
-        catList: ["Cuco", "Kico", "Garabato", "Locadio"],
-    }) );
+    var self = this;
+
+    this.catList = ko.observable([]);
+
+    initalCats.forEach(function(catItem){
+        self.catList().push(new Cat(catItem));
+    });
+
+    this.currentCat = ko.observable( self.catList()[0] );
 
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
+    };
+
+    this.changeCat = function() {
+        console.log("El bola");
     };
 };
 
